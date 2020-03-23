05d3e2f8b0985e88b7500ee7c1f8217ed058f774
x30unlimited <16005785+x30unlimited@users.noreply.github.com>
2020-03-23 12:18:35 +0200

    Update create_account.js

--- create_account.js
+++ create_account.js
@@ -3,7 +3,7 @@ const fs         = require('fs')
 const config     = JSON.parse(fs.readFileSync('./config.json'))
 const client     = new dsteem.Client('https://api.steemit.com')
 const active_key = dsteem.PrivateKey.fromString(config.active_key)
-const steem      = require('hive-stream');
+const steem      = require('steem');
 var utils        = require('./utils.js')
 
 //TODO: pass client as argument or as module export 
