5e55fa15c6d840686afcfacadff1dddfb13c2248
x30unlimited <16005785+x30unlimited@users.noreply.github.com>
2020-03-23 13:24:29 +0200

    Update postpromoter.js

--- postpromoter.js
+++ postpromoter.js
@@ -201,7 +201,7 @@ function startProcess() {
       getTransactions(saveState);
       
 	//test
-      utils.log("acc: " + JSON.stringify(account));
+      //utils.log("acc: " + JSON.stringify(account));
 	    
       // Check if there are any rewards to claim.
       claimRewards();
