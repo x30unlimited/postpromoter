0746a97bccf8b556270ff3254dbf95a7adb34d25
x30unlimited <16005785+x30unlimited@users.noreply.github.com>
2024-08-08 12:56:45 +0300

    small fix :P

--- postpromoter.js
+++ postpromoter.js
@@ -105,7 +105,6 @@ function startup() {
 	                    flex-direction: column;
 	                    align-items: center;
 	                    justify-content: center;
-	                    min-height: 100vh;
 	                    background-color: #f8f9fa;
 	                }
 	                .container {
@@ -138,12 +137,12 @@ function startup() {
 	        </head>
 	        <body>
 	            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	                <div class="container-fluid">
+	                <div class="container">
 	                    <a class="navbar-brand" href="#">Stats</a>
 	                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 	                        <span class="navbar-toggler-icon"></span>
 	                    </button>
-	                    <div class="collapse navbar-collapse" id="navbarNav">
+	                    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
 	                        <ul class="navbar-nav">
 	                            <li class="nav-item">
 	                                <a class="nav-link" href="#" id="vp">VP: </a>
